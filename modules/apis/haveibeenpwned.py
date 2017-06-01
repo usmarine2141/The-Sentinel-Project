7f64050a810e821a8ad50cd9b1dd729bc539984c
BlackVirusScript <BlackVirusScript@Tutanota.com>
2017-06-01 20:54:27 -0300

    Add files via upload

--- modules/apis/haveibeenpwned.py
+++ modules/apis/haveibeenpwned.py
@@ -4,9 +4,9 @@ import time, argparse
 
 __doc__ = "Single class module to interact with the HaveIBeenPwned (haveibeenpwned.com) API."
 
-class HIBP(session.Session):
+class API(session.Session):
     def __init__(self, version=2):
-        super(HIBP, self).__init__(f"https://haveibeenpwned.com/api/v{version}/")
+        super(API, self).__init__(f"https://haveibeenpwned.com/api/v{version}/")
         self.codes = {200: "Ok — Everything worked and there's a string array of pwned sites for the account.",
                       400: "Bad request — The account does not comply with an acceptable format. (i.e. it's an empty string)",
                       403: "Forbidden — No user agent has been specified in the request.",
